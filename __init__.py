ba6eac24dd3a4264ef563b921dce97eb23324b3f
hbudinoff <42483094+hbudinoff@users.noreply.github.com>
2023-03-02 11:22:32 -0700

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -5,20 +5,10 @@
 # PluginCostCalculator is released under the terms of the AGPLv3 or higher.
 
 from . import ElectricPrintCostCalculator
-from UM.i18n import i18nCatalog
-i18n_catalog = i18nCatalog("ElectricPrintCostCalculator")
+# from UM.i18n import i18nCatalog
+# i18n_catalog = i18nCatalog("ElectricPrintCostCalculator")
 def getMetaData():
-    return {
-        "type": "extension",
-        "plugin": 
-        {
-            "name": "Electrical Print Cost Calculator",
-            "author": "Zoff",
-            "version": "0.99.0",
-            "api": 3,
-            "description": i18n_catalog.i18nc("Description of plugin","Extension to quickly calculate print costs")
-        }
-    }
+    return {}
         
 def register(app):
     return { "extension": ElectricPrintCostCalculator.ElectricPrintCostCalculator()}
